7e4b552f979376eb72b7f1b67d0f77fe30a2daef
Heri <hlondono@misena.edu.co>
2021-09-29 07:23:21 -0500

    Actualizar el paciente

--- HospiEnCasa.App.Frontend/Pages/Pacientes/Edit.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Edit.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
+
 
 namespace HospiEncasa.App.Frontend.Pages.Pacientes
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPaciente _repoPaciente;
+        public Paciente paciente {get; set;}
+        public EditModel(IRepositorioPaciente repoPaciente)
         {
+            _repoPaciente = repoPaciente;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            paciente = _repoPaciente.GetPaciente(id);
+            if(paciente == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(Paciente paciente){
+            _repoPaciente.UpdatePaciente(paciente);
+            return RedirectToPage("Index");
         }
     }
 }
